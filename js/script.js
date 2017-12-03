8dd229173e4fad41542f2cd31ecb2143a293f3b4
joshrod <joshrodriguez17@gmail.com>
2017-12-03 17:50:17 -0600

    Completee styling for Snapchat case study, lay foundation for case study styling, modify sidebar for case study pages, remove js until all pages are completed

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ window.onload = function() {
 	/*VARIABLES*/
 	var sidebar = document.getElementsByClassName('sidebar')[0];
 	var sidebarLinks = sidebar.children;
+	var introLink = document.getElementsByClassName('intro-link')[0];
+	var introLine = document.getElementsByClassName('line')[0];
 
 	/*TOP OF SECTIONS TO SLIDE SIDEBAR AND SHOW ACTIVE TAB*/
 	var portfolioTop = document.getElementById('portfolio').getBoundingClientRect().top + window.pageYOffset;
@@ -14,11 +16,22 @@ window.onload = function() {
 		sidebar.classList.add('sidebar-active');
 	} else {
 		sidebar.classList.remove('sidebar-active');
-	} 
+	}
 
 	activeTabs();
 
+	/*EXTENDS LINE ON LOAD AND WHEN HOVERING OVER LINK IN THE INTRO*/
+	introLink.addEventListener('mouseover', function() {
+		introLine.classList.add('extend');
+	});
+	introLink.addEventListener('mouseout', function() {
+		introLine.classList.remove('extend');
+	});
+
+	/*** EVENT HANDLING ***/
+
 	window.onscroll = function() {
+
 		/*DISPLAY SIDEBAR WHEN TOP OF WINDOW SCROLLS PAST TOP OF PORTFOLIO DIV*/
 		if (window.pageYOffset >= portfolioTop) {
 			sidebar.classList.add('sidebar-active');
@@ -28,6 +41,9 @@ window.onload = function() {
 
 		activeTabs();
 	}
+
+	
+	/***EXTERNAL FUNCTIONS ***/
 
 	/*ONCE USER WINDOW REACHES DIV, SIDEBAR CHANGES ACTIVE TAB TO WHICHEVER TAB USER IS CURRENTLY ON*/
 	function activeTabs() {
